3d4641ad6450138aa12052ab1e2d4ea0c2217f7e
Валерий Дорохольский <c1mba@yandex.ru>
2020-11-09 10:20:05 +0300

    Правки миграции

--- app/Modules/Console/migrations/m201001_053756_create_metadata_table.php
+++ app/Modules/Console/migrations/m201001_053756_create_metadata_table.php
@@ -2,11 +2,10 @@
 
 use yii\db\Migration;
 
-class m201001_053756_createmetadata_table extends Migration
+class m201001_053756_create_metadata_table extends Migration
 {
     public function safeUp()
     {
-        $this->renameTable('{{%setting_page}}', '{{%meta_data}}');
         $this->createTable('{{%meta_data}}', [
             'action_id' => $this->primaryKey(),
             'title' => $this->string(255),
